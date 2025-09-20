e52d76c7bc71c7a71b6ca99321cdf54332caf106
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2025-09-20 18:07:39 +0530

    updated

---
+++ 09-datatypes copy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+SUM=$((NUMBER1 + NUMBER2))
+echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"   
+DIFF=$((NUMBER2 - NUMBER1))
+echo "Difference of $NUMBER2 and $NUMBER1 is: $DIFF"   
+PRODUCT=$((NUMBER1 * NUMBER2))
+echo "Product of $NUMBER1 and $NUMBER2 is: $PRODUCT"
+
+Leaders= ("Hemansh" "Vikram" "Ankit" "Rohit")
+echo "First Leader is: ${Leaders[0]}"
+echo "Second Leader is: ${Leaders[1]}"
+echo "All Leaders are: ${Leaders[@]}"
+echo "Total number of Leaders are: ${#Leaders[@]}"  
