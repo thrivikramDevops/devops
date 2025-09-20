2754ec615aafb493e98f6300e006b1b70a9b986d
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2025-09-20 17:37:27 +0530

    updated

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#passing arguments to script
+person1=S1
+Person2=$2
+
+echo " $person1 : Hello, $Person2! Welcome to DevOps shell scripting."
+echo " $Person2 : Hello, $person1! How are you?"
+echo " $person1 : I'm good, thank you! How about you?"
+echo " $Person2 : I'm doing well too. Thanks for asking!"
