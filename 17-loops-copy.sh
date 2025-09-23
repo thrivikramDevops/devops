c6b6cbdad50f1ad60c9017730f70d9e82a2c60be
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2025-09-23 13:51:54 +0530

    updated

--- 17-loops-copy.sh
+++ 17-loops-copy.sh
@@ -6,6 +6,9 @@ for i in {1..$NUMBER}; do
     for j in {1..$i}; do
         RESULT=1
         echo "$RESULT"
+        j=$((j + 1))
+        exit
     done
-    ((i++)) 
+    i=$((i + 1)
+    exit 0 
 done
