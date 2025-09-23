edeac1c26c038ae507102705e8a0cdc8fe92ef3f
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2025-09-23 12:09:56 +0530

    updated

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -4,8 +4,8 @@ echo "Enter  number to check even or odd"
 read Number
 if( $Number%2 -eq 0)
 then 
-    echo " Given Number is Even"
+    echo " Given $Number is Even"
     else
-    echo " Given Number is Odd"
+    echo " Given $Number is Odd"
 fi
 
