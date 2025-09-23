7c6be4e1e228969e84ebca3568d7cfda271fb06d
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2025-09-23 12:38:58 +0530

    updated

---
+++ 12-installation copy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE()
+{
+    if [ $? -ne 0 ]; then
+    echo "ERROR:: Installing MySQL is failure"
+    exit 1
+else
+    echo "Installing MySQL is SUCCESS"
+fi
+}
