fbfa4434e469d2dfa4038a816f9c53979823e149
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2025-09-23 12:56:30 +0530

    updated

--- 14-colors.sh
+++ 14-colors.sh
@@ -9,7 +9,7 @@ C="\e[36m" # Cyan
 W="\e[37m" # White
 N="\e[0m"  # No Color
 
-#R="\e[31m"
+    #R="\e[31m"
 #G="\e[32m"
 #Y="\e[33m"
 #N="\e[0m"
@@ -18,7 +18,7 @@ echo -e "${R}This is Red Color${N}"
 echo -e "${G}This is Green Color${N}"
 echo -e "${Y}This is Yellow Color${N}"
 echo -e "${B}This is Blue Color${N}"
-echo -e "${M}This is Magenta Color${N}"
+echo -e "${N}This is Magenta Color${N}"
 echo -e "${C}This is Cyan Color${N}"            
 echo -e "${W}This is White Color${N}"
 echo -e "${N}This is No Color${N}"
