dd6871d2ed5b34b43db5c0c78fd3df659f2e0b8a
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2025-09-23 13:06:22 +0530

    updated

---
+++ 15-colours-copy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+R='\e[31m' # Red
+G='\e[32m' # Green
+Y='\e[33m' # Yellow
+B='\e[34m' # Blue
+M='\e[35m' # Magenta
+C='\e[36m' # Cyan
+W='\e[37m' # White
+N='\e[0m'  # No Color
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo -e "${R}ERROR:: Please run this script with root privelege${N}"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "${R}ERROR:: Installing $2 is failure${N}"
+        exit 1
+    else
+        echo -e "{G}Installing $2 is SUCCESS"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install python3 -y
+VALIDATE $? "python3"
