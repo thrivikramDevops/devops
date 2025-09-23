a6c148bfa35f95b33881be464f5b3734c26e5b94
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2025-09-23 12:06:08 +0530

    updated

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -5,7 +5,7 @@ NUMBER=$1
 if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is less than 10"
     exit 1
-elseif [ $NUMBER -eq 10 ]; then
+elif [ $NUMBER -eq 10 ]; then
     echo "Given number $NUMBER is equal to 10"
 else
     echo "Given number $NUMBER is greater than  10"
