788a805f0479fec27179da445827eaf34ef6dc8e
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2025-09-23 13:09:52 +0530

    updated

--- 15-colours-copy.sh
+++ 15-colours-copy.sh
@@ -21,7 +21,7 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
         echo -e "${R}ERROR:: Installing $2 is failure${N}"
         exit 1
     else
-        echo -e "${B}Installing $2 is SUCCESS${N}"
+        echo -e "${C}Installing $2 is $G SUCCESS"
     fi
 }
 
